a162fa3bd0369a08b542c8da58ae9607f91fd237
Ethan <163362947+gobuldafish@users.noreply.github.com>
2026-09-13 15:46:18 -0400

    edit script.js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-=======
 //Variable that initializes and declares the human score
 
 //Variable that initializes and declares the computer score
@@ -21,7 +19,6 @@ scissorsBtn.addEventListener("click", () => {
     playRound('scissors', getComputerChoice())
 });
 
->>>>>>> rps-ui
 function getComputerChoice() {
     //Random variable that is from 1-3
     const randomNumber = Math.floor(Math.random() * 3) + 1;
@@ -42,57 +39,12 @@ function getHumanChoice() {
     const humanChoice = prompt("Rock, Paper, or Scissors?");  
     return humanChoice;
 }
-<<<<<<< HEAD
-//Variable that initializes and declares the human score
-
-//Variable that initializes and declares the computer score
-let humanScore = 0;
-let computerScore = 0;
 
 
-function playGame() {
-    
-    humanScore = 0;
-    computerScore = 0;
-    //Loop that plays five rounds
-    for(let i = 0; i < 5; i++) {
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-        playRound(humanSelection, computerSelection);
-    }
-    if (humanScore > computerScore) {
-        console.log("You win! Congratulations!");
-    } else if (humanScore < computerScore) {
-        console.log("Womp womp. You lose!");
-    } else {
-        console.log("It's a tie!");
-    }
-    
-    
-
-}
-=======
-
-
->>>>>>> rps-ui
 function playRound(humanChoice, computerChoice) {
     //Make humanChoice case-insensitive
     humanChoice = humanChoice.toLowerCase();
     //Conditional that takes the human choice of rock and compares it to the computerChoices
-<<<<<<< HEAD
-    if(humanChoice === 'rock') {
-        switch(computerChoice) {
-            case 'rock':
-                console.log("Draw!");
-                break;
-            case 'paper':
-                console.log("You lose! Paper beats Rock.");
-                computerScore++;
-                break;
-            case 'scissors':
-                console.log("You win! Rock beats Scissors.");
-                humanScore++;
-=======
     
     if(humanChoice === 'rock') {
         switch(computerChoice) {
@@ -112,24 +64,12 @@ function playRound(humanChoice, computerChoice) {
                 display.textContent = "You win! Rock beats Scissors.";
                 hScrDisplay.textContent = humanScore;
                 cScrDisplay.textContent = computerScore;
->>>>>>> rps-ui
                 break;
         
         }
     } else if (humanChoice === 'paper') {
         switch(computerChoice) {
             case 'rock':
-<<<<<<< HEAD
-                console.log("You win! Paper beats Rock.");
-                humanScore++;
-                break;
-            case 'paper':
-                console.log("Draw!");
-                break;
-            case 'scissors':
-                console.log("You lose! Scissors beats Paper.");
-                computerScore++;
-=======
                 humanScore++;
                 display.textContent = "You win! Paper beats Rock.";
                 hScrDisplay.textContent = humanScore;
@@ -145,27 +85,12 @@ function playRound(humanChoice, computerChoice) {
                 display.textContent = "You lose! Scissors beats Paper.";
                 hScrDisplay.textContent = humanScore;
                 cScrDisplay.textContent = computerScore;
->>>>>>> rps-ui
                 break;
         
         }
     } else {
         switch(computerChoice) {
             case 'rock':
-<<<<<<< HEAD
-                console.log("You lose! Rock beats Scissors.");
-                computerScore++;
-                break;
-            case 'paper':
-                console.log("You win! Scissors beats Paper.");
-                humanScore++;
-                break;
-            case 'scissors':
-                console.log("Draw!");
-                break;
-        }
-    }
-=======
                 computerScore++;
                 display.textContent = "You lose! Rock beats Scissors.";
                 hScrDisplay.textContent = humanScore;
@@ -193,15 +118,10 @@ function playRound(humanChoice, computerChoice) {
         humanScore = 0;
         computerScore = 0;
     }
->>>>>>> rps-ui
     //Conditional that takes the human choice of paper and compares it to the computerChoices
     //Conditional that takes the human choice of scissors and compares it to the computerChoices
     //Increment humanScore if human wins
     //Increment computerScore if computer wins
-<<<<<<< HEAD
-}
-=======
 }
 
 
->>>>>>> rps-ui
